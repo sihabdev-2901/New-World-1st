6e1021b8c2b3bdfef9e0ff6804ae8b3544441b96
sihabdev-2901 <a.h.sihab2901@gmail.com>
2026-09-11 16:34:06 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Hello, World!");
+document.write("Hello, World!");
+alert("Hello, World! You have entered NEW WORLD!");
+let nickname = prompt("Please enter your nickname:");
+console.log("Hello, " + nickname + "!");
+document.write("Hello, " + nickname + "! Welcome to the NEW WORLD!");
+var age = prompt("Please enter your age:");
+document.write("You are " + age + " years old.");
